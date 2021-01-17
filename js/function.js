969f890df81c2a5ee07e2b3c329e9a72e76ef4a5
GEN3987 <66622502+GEN3987@users.noreply.github.com>
2021-01-17 15:13:51 +0900

    Create function.js

---
+++ js/function.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('#vegas').vegas({
+        slides: [
+            { src: 'img/1.png' },
+            { src: 'img/2.jpg' },
+            { src: 'img/3.jpg' }
+        ],
+    });
+});
